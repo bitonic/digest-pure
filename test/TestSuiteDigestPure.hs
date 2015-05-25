94508c004a7bef4ffddcf5dc408ad12cf32c5db5
Francesco Mazzoli <f@mazzo.li>
2015-05-25 22:11:47 +0200

    Compute CRC32 word-by-word instead of byte-by-byte.
    This makes the Haskell algorithm only 20% slower than the C one if
    the input is greater than a kilobyte.  The algorithm itself is
    from zlib.

--- test/TestSuiteDigestPure.hs
+++ test/TestSuiteDigestPure.hs
@@ -1,22 +1,29 @@
 {-# LANGUAGE DoAndIfThenElse #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Digest.Adler32 as A
 import qualified Data.Digest.CRC32 as C
 import qualified Data.Digest.Pure.Adler32 as AP
 import qualified Data.Digest.Pure.CRC32 as CP
+import Data.Functor ((<$>))
 import System.Exit
 import Test.QuickCheck
 import Text.Printf
 
 instance Arbitrary B.ByteString where
-  arbitrary = do
-    n <- choose (1, 1000000)
-    l <- vector n
-    return $ B.pack l
+  arbitrary = B.pack <$> arbitrary
+  shrink xs = B.pack <$> shrink (B.unpack xs)
 
+instance Arbitrary BL.ByteString where
+  arbitrary = BL.pack <$> arbitrary
+  shrink xs = BL.pack <$> shrink (BL.unpack xs)
+
+main :: IO ()
 main = do
   results <- mapM (\(s, a) -> printf "%-25s: " s >> a) tests
   if all succesful results then
@@ -28,11 +35,15 @@ succesful :: Result -> Bool
 succesful (Success _ _ _) = True
 succesful _               = False
 
+tests :: [([Char], IO Result)]
 tests = [
-  ("adler32CompareZlib", quickCheckResult adler32CompareZlib),
-  ("adler32Update",      quickCheckResult adler32Update),
-  ("crc32CompareZlib",   quickCheckResult crc32CompareZlib),
-  ("crc32Update",        quickCheckResult crc32Update) ]
+  ("adler32CompareZlib",   quickCheckResult adler32CompareZlib),
+  ("adler32Update",        quickCheckResult adler32Update),
+  ("crc32CompareZlib",     quickCheckResult crc32CompareZlib),
+  ("crc32CompareZlibLazy", quickCheckResult crc32CompareZlibLazy),
+  ("crc32Update",          quickCheckResult crc32Update),
+  ("crc32Unaligned",       quickCheckResult crc32Unaligned)
+ ]
 
 adler32CompareZlib :: B.ByteString -> Bool
 adler32CompareZlib str =
@@ -48,9 +59,18 @@ crc32CompareZlib :: B.ByteString -> Bool
 crc32CompareZlib str =
   CP.crc32 str == C.crc32 str
 
+crc32CompareZlibLazy :: BL.ByteString -> Bool
+crc32CompareZlibLazy str =
+  CP.crc32 str == C.crc32 str
+
 crc32Update :: B.ByteString -> B.ByteString -> Bool
 crc32Update str1 str2 =
   CP.crc32 strConcat == CP.crc32Update (CP.crc32 str1) str2
   where
     strConcat = B.append str1 str2
 
+crc32Unaligned :: B.ByteString -> Gen Bool
+crc32Unaligned str0 = do
+  n <- choose (1, 3)
+  let str = B.drop n str0
+  return $ CP.crc32 str == C.crc32 str
